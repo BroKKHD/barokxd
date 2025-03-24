157f5f5170143b441f6bb23cf7d9092b5c14957f
BroKKHD <brokhdfall@gmail.com>
2025-03-24 17:03:15 +0300

    script.js

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
-button {
-    width: 150px;
-    height: 60px;
-    font-size: 18px;
-    border: none;
-    cursor: pointer;
-    border-radius: 10px;
-    position: absolute;
-    bottom: 20px;
-    transition: transform 0.2s ease-in-out; /* Buton kayması */
-    pointer-events: auto; /* Buton tıklanabilir */
-}
+const button = document.getElementById("kaybolan");
 
-button:hover {
-    transform: translateX(20px); /* Mouse üzerine geldiğinde buton kayacak */
-}
-
-.strawberry {
-    position: absolute;
-    cursor: pointer;
-    width: 80px;
-    height: 80px;
-    display: none;
-    transition: 0.3s ease;
-}
-
-.strawberry-message {
-    position: absolute;
-    top: 90px;
-    width: 300px;
-    text-align: center;
-    color: white;
-    font-size: 18px;
-    display: none;
-}
-
-.messages {
-    position: absolute;
-    top: 100px;
-    width: 300px;
-    text-align: center;
-    color: white;
-    font-size: 18px;
-}
+button.addEventListener("mouseover", function() {
+    const x = Math.random() * (window.innerWidth - button.offsetWidth);
+    const y = Math.random() * (window.innerHeight - button.offsetHeight);
+    button.style.position = "absolute";
+    button.style.left = `${x}px`;
+    button.style.top = `${y}px`;
+});
