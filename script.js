ea7a1d1b1a54afd6149f8ee03bcca738e73bd248
BroKKHD <brokhdfall@gmail.com>
2025-03-24 05:11:52 +0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("kaybolan").addEventListener("mouseover", function() {
-    let button = this;
-    let x = Math.random() * (window.innerWidth - button.offsetWidth);
-    let y = Math.random() * (window.innerHeight - button.offsetHeight);
-    
+const button = document.getElementById("kaybolan");
+
+button.addEventListener("mouseover", function() {
+    const x = Math.random() * (window.innerWidth - button.offsetWidth);
+    const y = Math.random() * (window.innerHeight - button.offsetHeight);
     button.style.position = "absolute";
     button.style.left = `${x}px`;
     button.style.top = `${y}px`;
